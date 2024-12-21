4ae85082022662773afa79055a077df77bc4763a
Gideon <eignnx@gmail.com>
2024-12-21 14:28:52 -0500

    get cli working again

--- run.sh
+++ run.sh
@@ -4,14 +4,12 @@ set -e
 SRC=$(basename -s .meadow "$1")
 
 # Compile
-cd ./meadowlark
-time cargo run "../$1" > "../$SRC.lark"
+cargo run --bin meadowlark -- "../$1" > "../$SRC.lark"
 
 cd ..
 
 # Assemble
-time customasm "$SRC.lark"
+customasm "$SRC.lark"
 
 # Run
-cd ./lark-vm
-time cargo run $2 "../$SRC.bin"
+cargo run --bin lark-ui -- $2 "../$SRC.bin"
